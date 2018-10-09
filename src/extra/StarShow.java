2539c5ab9e009eff02c486281a7d96f39a3b21c4
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-08 17:49:41 -0700

    october 8 2018

--- src/extra/StarShow.java
+++ src/extra/StarShow.java
@@ -17,26 +17,58 @@ public class StarShow {
 	
 	void makeStars() {
 		
-		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
+			//5. delete this line. you will draw the star again in step 8.
+		
 		// 13. Set the speed to 8
 
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+              int xpos = 10; 
 		// 7. Make a variable to hold the Y position of the Robot and set it to 950
-
+                int ypos = 950;
 		// 8. Make a variable to hold the star size and set it to 25
+int star = 25;
+		// 12. Repeat the steps #19 to #18, 30 times
 
+			// 19. Set the pen wpackage extra;
+
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MoveAction;
+
+import org.jointheleague.graphical.robot.Robot;
+
+/*** Teacher's note ***/
+/* Before beginning recipe: 
+* 1. ask students to find and explain the method in this recipe. 
+* 2. ask students how they might use the method to make the picture in the laminated hand-outs. */
+
+public class StarShow {
+	
+	Robot robot = new Robot("batman");
+	
+	void makeStars() {
+		
+			//5. delete this line. you will draw the star again in step 8.
+		
+		// 13. Set the speed to 8
+
+		// 6. Make a variable to hold the X position of the Robot and set it to 10
+              int xpos = 10; 
+		// 7. Make a variable to hold the Y position of the Robot and set it to 950
+                int ypos = 950;
+		// 8. Make a variable to hold the star size and set it to 25
+int star = 25;
 		// 12. Repeat the steps #19 to #18, 30 times
 
 			// 19. Set the pen width to i 
 	
 			// 10. Set the X position of the robot to your X variable
-	
+	          
 			// 11. Set the Y position of the robot to your Y variable
 	
 			// 9. Call the drawStar() method with your star size variable
-	
-			// 14. Increase the X position by star size. See Figure 2.
+	  drawStar(star);
+		// 14. Increase the X position by star size. See Figure 2.
 	
 			// 15. decrease the Y position by star size. See Figure 3.
 	
@@ -48,13 +80,17 @@ public class StarShow {
 
 	}
 
-	private void drawStar(int starSize) {
+	private void drawStar(int stardistanceSize) {
 		// 2. Put the robot's pen down
             robot.penDown();
+            robot.setSpeed(100);
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
-
+                    for (int i = 0; i < 5; i++) {
+                    	robot.move(starSize);	
+                    	robot.turn(144);
+					}
 			// 1. Move the robot the distance of the starSize variable
-	robot.move(starsize);
+	robot.move(starSize);
 			// 3. Turn the robot 144 degrees
 			robot.turn(144);
 	}
@@ -67,4 +103,43 @@ public class StarShow {
 
 
 
+idth to i 
+	
+			// 10. Set the X position of the robot to your X variable
+	          
+			// 11. Set the Y position of the robot to your Y variable
+	
+			// 9. Call the drawStar() method with your star size variable
+	  drawStar(star);
+		// 14. Increase the X position by star size. See Figure 2.
+	
+			// 15. decrease the Y position by star size. See Figure 3.
+	
+			// 16. Increase the star size by 20ak("i like eating cats with gravy
+	
+			// 17. Turn the robot 12 degrees
+	
+			// 18. Make each star a different random color like in Figure 4. 
+
+	}
+
+	private void drawStar(int stardistanceSize) {
+		// 2. Put the robot's pen down
+            robot.penDown();
+            robot.setSpeed(100);
+		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
+                    for (int i = 0; i < 5; i++) {
+                    	robot.move(starSize);	
+                    	robot.turn(144);
+					}
+			// 1. Move the robot the distance of the starSize variable
+	robot.move(starSize);
+			// 3. Turn the robot 144 degrees
+			robot.turn(144);
+	}
+	
+	public static void main(String[] args) {
+		new StarShow().makeStars();
+	}
+}
 
