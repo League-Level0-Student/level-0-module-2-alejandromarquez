cecb90b5b4c91ee0a3bde92b525531b51681efc9
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-15 17:48:29 -0700

    10/14/18

--- rollarcoaster.java
+++ rollarcoaster.java
@@ -2,41 +2,23 @@ import javax.swing.JOptionPane;
 
 public class rollarcoaster {
 
-	JOptionPane.
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		String roller = JOptionPane.showInputDialog("What is your height");
+		int coaster =Integer.parseInt(roller);
+	if (coaster is greater than 48) {
+		
+	}
+		
+		
+		
+		
+		
+		
+	}
 	
 }
