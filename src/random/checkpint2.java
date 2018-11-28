d3921a61f849080ce53f2fd5f4a50056c923c723
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-27 17:42:07 -0800

    11/26/18

---
+++ src/random/checkpint2.java
@@ -0,0 +1,52 @@
+package random;
+
+import java.util.Random;
+
+public class checkpint2 {
+public static void main(String[] args) {
+	
+	
+	
+	
+	int Doge = 0;	
+	
+	
+	 Random jaVa = new Random();
+	  Doge = jaVa.nextInt(900-0+1)+0;
+	  System.out.println(Doge);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
---
+++ src/random/checkpoint.java
@@ -0,0 +1,108 @@
+package random;
+
+import java.util.Random;
+
+public class checkpoint {
+public static void main(String[] args) {
+	
+	
+	int Kat = 0;	
+	
+	
+		
+	  Random jaVa = new Random();
+	  Kat = jaVa.nextInt(1000-0+1)+0;
+	  System.out.println(Kat);
+	
+	
+	
+	
+	
+	
+	  int Doge = 0;	
+		  Doge = jaVa.nextInt(900-0+1)+0;
+		  System.out.println(Doge);
+	
+	
+	
+	
+	
+	System.out.println(Doge-Kat);
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
