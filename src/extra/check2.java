0d68dcb2045d221e0f25008f6336342849f0ff6e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-03 17:43:58 -0800

    5:43

---
+++ src/extra/check2.java
@@ -0,0 +1,9 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class check2 {
+public static void main(String[] args) {
+	JOptionPane.showInputDialog("how many cups of flour do you have");
+}
+}
